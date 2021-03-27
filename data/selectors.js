70ee0ebada33d8aa5e68b099cd64856827a89c6e
MaksymPashkovskyi <pashkovskymax.mp@gmail.com>
2021-03-27 12:40:39 +0200

    remove gitignore

--- data/selectors.js
+++ data/selectors.js
@@ -1,7 +1,7 @@
 const elements = {
     "header": "h3",
     "instruction": "p",
-    "requiredLabel": ".ant-form-item-required", // "label[for=name]","label[for=age]","label[for=gender]"
+    "requiredLabel": ".ant-form-item-required",
     "name": "#name",
     "radioButtons": ".ant-radio",
     "radioButtonsLabel": ".ant-radio-wrapper",
